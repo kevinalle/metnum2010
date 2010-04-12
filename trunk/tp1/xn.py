a95fe048b489935d64eda36d560407ca34a7b788
pablodherrero@gmail.com <pablodherrero@gmail.com@8062f241-9d54-ddaa-24e5-4d4b1e181026>
2010-04-12 05:25:14 +0000

    python impl

--- trunk/tp1/xn.py
+++ trunk/tp1/xn.py
@@ -2,13 +2,17 @@
 
 from itertools import *
 
-def alpha_beta(x0, x1):# gamma == 0
-	mat = [ [ , 0], [ , 0] ]
+def alpha_beta_gamma(x0, x1):# gamma != 0
+	mat = [ [ 3-xo  ,   5-x0,     100-x0        ],
+			[ 3*(3-x1), 5*(5-x0), (100)*100-x0] ] ]
+			
 	triangular(mat)
 	
+	b = -mat[1][2]/mat[1][1]
+	a = -mat[0][2]/mat[0][0] + b*mat[0][1]/mat[0][0]
+	return a, b
 		
 
-# Precondicion: mat es cuadrada
 def triangular(mat):
 	n = len(mat)
 	for i in xrange(0, n-1):
@@ -16,10 +20,10 @@ def triangular(mat):
 		coefs = [ l[i] for l in mat[i:n] ]
 		idx = coefs.index(max(coefs))
 		mat[i], mat[idx] = mat[idx], mat[i]
-		#Pivoteaer con es fila
+		#Pivotear con esa fila
 		if mat[i][i] != 0:
 			for k in xrange(i+1,n):
-				mat[k][i:n] = calc_fila(mat[i][i:n], mat[k][i:n])
+				mat[k][i:] = calc_fila(mat[i][i:], mat[k][i:])
 
 	
 def calc_fila(l1, l2):
@@ -32,6 +36,16 @@ def x_n(n,x1,x0):
 	elif n == 1: return x1
 	else:
 		return 108 - (815 - (1500/x_n(n-2,x1,x0)))/x_n(n-1,x1,x0)
+		
+def x_n_iter(n,x1,x0):
+	if n >= 1:
+		res,res_1 = x1, x0
+	else: res = x0
+	
+	for i in xrange(1,n):
+		res += 108 - (815 - (1500/res_1))/res
+		
+	return res
 		
 
 

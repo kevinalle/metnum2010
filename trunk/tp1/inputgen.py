99b3fb60146100d7a9796572f73afa4b856ab089
puedovolar <puedovolar@8062f241-9d54-ddaa-24e5-4d4b1e181026>
2010-04-14 15:13:00 +0000

    inputgen de python

---
+++ trunk/tp1/inputgen.py
@@ -0,0 +1,21 @@
+#! /usr/bin/python
+
+def exacto(x0,x1):
+	# si gamma puede ser 0
+	if (x1-8)*x0 + 15 < 0.1:
+		# si b==0 y a!=0
+		if x0==3 and x1!=5:
+			return 3
+		# si b!=0
+		else:
+			return 5
+	# si gamma !=0
+	else:
+		return 100
+	
+		
+
+r = [float(i)/100 for i in range(-1000,1001)]
+for x0 in r:
+	for x1 in r:
+		print str(x0) + " " + str(x1)# + " " + str(exacto(x0,x1))

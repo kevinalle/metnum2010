0cbd7bab9c26069cedab00280cb6b93c48eb6886
kevinalle <kevinalle@8062f241-9d54-ddaa-24e5-4d4b1e181026>
2010-04-14 17:48:35 +0000

    syntax error

--- trunk/tp1/toCSV.py
+++ trunk/tp1/toCSV.py
@@ -1,1 +1,2 @@
-ofile=open('xn.csv','w'); for line in open('math.out').read().split("\n"): ofile.write(','.join(line.split())+"\n")
+ofile=open('xn.csv','w');
+for line in open('math.out').read().split("\n"): ofile.write(','.join(line.split())+"\n")

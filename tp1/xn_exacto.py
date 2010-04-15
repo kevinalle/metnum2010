cd0ff73215da700522fc11b38363e72bce9ab819
kevinalle <kevinalle@8062f241-9d54-ddaa-24e5-4d4b1e181026>
2010-04-15 20:10:08 +0000

    arregle xn_exacto.py

--- tp1/xn_exacto.py
+++ tp1/xn_exacto.py
@@ -2,21 +2,15 @@
 
 e = 0.00001
 
-def pow(n,m):
-	if m==0: return 1
-	if m==1: return n
-	else:
-		return n*pow(n,m-1)
-
 def xn_exacto(x0,x1,n):
 	# si gamma puede ser 0
-	if (x1-8)*x0 + 15 < e:
+	if abs((x1-8)*x0 + 15) < e:
 		# elijo un b
 		b = 1
 		# calculo a
 		a = b*(5-x0)/(x0-3)
 		# calculo la sucesion hasta n
-		return [ ( a*pow(3,i+1) + b*pow(5,i+1) ) / ( a*pow(3,i) + b*pow(5,i) ) for i in range(2,n) ]
+		return [ ( a*3**(i+1) + b*5**(i+1) ) / ( a*3**i + b*5**i ) for i in range(2,n) ]
 	# si gamma != 0
 	else:
 		# calculo b/g
@@ -24,4 +18,6 @@ def xn_exacto(x0,x1,n):
 		#calculo a/g
 		ag = (10000-100*x0-bg*(x0-5)) / (x0-3)
 		# calculo la sucesion hasta n
-		return [ ( ag*pow(3,i+1) + bg*pow(5,i+1) + pow(100,i+1) ) / ( ag*pow(3,i) + bg*pow(5,i) + pow(100,i) ) for i in range(2,n) ]
+		return [ ( ag*3**(i+1) + bg*5**(i+1) + 100**(i+1) ) / ( ag*3**i + bg*5**i + 100**i ) for i in range(2,n) ]
+
+print xn_exacto(4.,4.25,20)

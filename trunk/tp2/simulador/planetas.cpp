d34fbcc8cf79ae0c605124378af019c01dc08197
puedovolar <puedovolar@8062f241-9d54-ddaa-24e5-4d4b1e181026>
2010-05-05 06:10:48 +0000

    un poco menos fruta, pero fruta al fin

--- trunk/tp2/simulador/planetas.cpp
+++ trunk/tp2/simulador/planetas.cpp
@@ -13,7 +13,7 @@ typedef vector<vd> vvd;
 
 SDL_Surface* screen;
 
-#define G 6.67428e-11
+#define G 0.00002499215588275752801651213378056900054016
 
 struct Planeta{
 	Vector x;
@@ -109,32 +109,32 @@ int main(){
 
 	// Coordenada (0,0): Solar System Barycenter
 	// datos para: A.D. 2010-May-05 00:00:00.0000 CT
+	// G = 6,67428 * 10^-11 * m^3 / (Kg*s^2)
+	// = 0.00002499215588275752801651213378056900054016 * AU^3 / (1e-30*Kg * dia^2)
 	// unidades:
 	// * distancia: AU	= 149597870.691 km
 	// * tiempo: dias	= 86400 s
-	// * peso: Kg
-
-	double PP = 10000.;
+	// * peso: 1.e-30 Kg
 
 	Planeta sol;
 	sol.x = Vector(-4.042895106228434e-03, 2.209532530800580e-03, 1.136425407067405e-05);	// AU
 	sol.v = Vector(-1.786236608923230e-06, -5.959144368171789e-06, 4.287462284225408e-08);	// AU / dia
-	sol.m = PP*1.; // 10^30 Kg
+	sol.m = 1; // 10^30 Kg
 
 	Planeta tierra;
 	tierra.x = Vector(-7.262551982062518e-01, -7.016177342125620e-01, 2.942601923402196e-05);	// AU
 	tierra.v = Vector(1.172032570433690e-02, -1.239499064419012e-02, -2.875393960566639e-07);	// AU / dia
-	tierra.m = PP*1.e-6; // 10^24 Kg = m_sol/10^6
+	tierra.m = 1e-6; // 10^24 Kg = m_sol/10^6
 
 	Planeta luna;
-	luna.x = Vector(-7.248557601516714e-01, -7.039097848869444e-01, 1.029051462977911e-04);
-	luna.v = Vector(1.220576787081027e-02, -1.211595432894550e-02, 4.775108508860639e-05);
-	luna.m = PP*1.e-10; // 10^20 Kg = m_sol/10^10
+	luna.x = Vector(-7.248557601516714e-01, -7.039097848869444e-01, 1.029051462977911e-04);	// AU
+	luna.v = Vector(1.220576787081027e-02, -1.211595432894550e-02, 4.775108508860639e-05);	// AU / dia
+	luna.m = 1e-10; // 10^20 Kg = m_sol/10^10
 
 	Observador o;
 //	o.pos = Vector(0,0,0);
-	o.dir = Vector(0,1,0).normalize();//o.pos.inverse().normalize();
-	o.up = Vector(0,0,1).normalize();
+	o.dir = Vector(1,0,0).normalize();//o.pos.inverse().normalize();
+	o.up = Vector(0,-1,0).normalize();
 
 	clear(screen,depth);
 	draw_base(screen,depth,o);
@@ -187,7 +187,6 @@ int main(){
 
 		// fuerza gravitacional ejercida sobre p1 por p2:
 		// Fg(p1,p2) = -G*m1*m2 * (x1-x2)/(|x1-x2|^3)
-		// G = 6,67428 * 10^-11 * m^3 / (Kg*s^2)
 
 		Vector F_l = Fg(luna,tierra) + Fg(luna,sol);
 		Vector F_t = Fg(tierra,sol) + Fg(tierra,luna);
@@ -205,7 +204,7 @@ int main(){
 		tierra.x += tierra.v;
 		sol.x += sol.v;
 
-		draw(screen,depth,o,luna.x,255,0,255);
+		draw(screen,depth,o,luna.x,0,255,255);
 		draw(screen,depth,o,tierra.x,255,255,255);
 		draw(screen,depth,o,sol.x,255,255,0);
 

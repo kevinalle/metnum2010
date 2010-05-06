0afbb4c5bb46011d70027459a46c7c1472db26fe
puedovolar <puedovolar@8062f241-9d54-ddaa-24e5-4d4b1e181026>
2010-05-06 17:48:51 +0000

    un par de planetas mas y dt

--- tp2/simulador/Planetas.h
+++ tp2/simulador/Planetas.h
@@ -8,7 +8,7 @@
 	// unidades:
 	// * distancia: AU	= 149597870.691 km
 	// * tiempo: dias	= 86400 s
-	// * peso: 1.e-30 Kg
+	// * peso: 1.e30 Kg
 
 struct Planeta{
 	Vector x;
@@ -27,6 +27,14 @@ void initPlanetas(){
 	sol.v = Vector(-1.786236608923230e-06, -5.959144368171789e-06, 4.287462284225408e-08);	// AU / dia
 	sol.m = 1.9891; // 1.9891 * 10^30 Kg
 
+	mercurio.x = Vector(-2.563687782457914E-01, -3.810998677594219E-01, -8.153193298845162E-03);
+	mercurio.v = Vector(1.781451838572982E-02, -1.414097937982711E-02, -2.789632381177015E-03);
+	mercurio.m = 0.0000003302; // 3.302 * 10^23
+
+	venus.x = Vector(-2.745105890947935E-01, 6.675955871621595E-01, 2.473298509458710E-02);
+	venus.v = Vector(-1.880800003531344E-02, -7.736184882354628E-03, 9.795183384655537E-04);
+	venus.m = 0.0000048685; // 48.685 * 10^23 Kg
+
 	tierra.x = Vector(-7.262551982062518e-01, -7.016177342125620e-01, 2.942601923402196e-05);	// AU
 	tierra.v = Vector(1.172032570433690e-02, -1.239499064419012e-02, -2.875393960566639e-07);	// AU / dia
 	tierra.m = 0.0000059736; // 5.9736 * 10^24 Kg
@@ -36,6 +44,8 @@ void initPlanetas(){
 	luna.m = 0.00000007349; // 734.9 * 10^20 Kg = m_sol/10^10
 
 	planetas.push_back(sol);
+	planetas.push_back(mercurio);
+	planetas.push_back(venus);
 	planetas.push_back(tierra);
 	planetas.push_back(luna);
 

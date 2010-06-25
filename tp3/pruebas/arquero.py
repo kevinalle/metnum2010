e50b66b6849c06647d4a833c6db0559d85515ff8
kevinalle <kevinalle@8062f241-9d54-ddaa-24e5-4d4b1e181026>
2010-06-25 02:31:07 +0000

    posta

--- tp3/pruebas/arquero.py
+++ tp3/pruebas/arquero.py
@@ -36,7 +36,7 @@ tests=[
 'test_ruido3.txt',
 'test_ruido4.txt']
 carpeta='../Tp_3_Mundial2010v1/tests/'
-test=7
+test=4
 pnts=map(lambda x: map(lambda y: float(y) if '.' in y else int(y),x.split()),open(carpeta+tests[test]).read().split("\n")[:-1])
 
 n=1
@@ -108,6 +108,46 @@ def metodo_spline(ts,xs,ys):
 	elif gol<arq-step/2 and lastdir!=1 and arq>step/2-1: arq-=step; lastdir=-1
 	else: lastdir=0
 
+def metodo_CM(ts,xs,ys):
+	global arq,lastdir
+	cx=cuadmin(ts,xs,3)
+	cy=cuadmin(ts,ys,3)
+	xs=[poli(cx,t) for t in ts]
+	ys=[poli(cy,t) for t in ts]
+	plot(zip([poli(cx,t) for t in ts+range(ts[-1],ts[-1]+40)], [poli(cy,t) for t in ts+range(ts[-1],ts[-1]+40)]),red)
+	
+	t=ts[-1]
+	y=ys[-1]
+	yans=ys[-2]
+	while y>0 and t<200:
+		t+=1
+		yans=y
+		y=poli(cy,t)
+		#if yans<y: text("whops",pos=(100,100)); break;
+	gol=poli(cx,t)#+y/(yans-y))
+	if gol>arq+step/2 and lastdir!=-1 and arq<1-step/2: arq+=step; lastdir=1
+	elif gol<arq-step/2 and lastdir!=1 and arq>step/2-1: arq-=step; lastdir=-1
+	else: lastdir=0
+
+def metodo_CM2(ts,xs,ys):
+	global arq,lastdir
+	#eta=ys[0]/(sum(ys[i]-ys[i+1] for i in range(len(ys)-1))/(len(ys)-1))
+	cx=cuadmin(ts[-5:],xs[-5:],2)
+	cy=cuadmin(ts[-5:],ys[-5:],2)
+	plot([(poli(cx,t),poli(cy,t)) for t in ts[-5:]+range(ts[-1],ts[-1]+40)],red)
+	
+	t=ts[-1]
+	y=ys[-1]
+	yans=ys[-2]
+	while y>0 and t<200:
+		t+=1
+		yans=y
+		y=poli(cy,t)
+	gol=poli(cx,t+y/(yans-y))
+	if gol>arq+step/2 and lastdir!=-1 and arq<1-step/2: arq+=step; lastdir=1
+	elif gol<arq-step/2 and lastdir!=1 and arq>step/2-1: arq-=step; lastdir=-1
+	else: lastdir=0
+
 def do():
 	screen.fill(bg)
 	global arq,lastdir
@@ -115,7 +155,9 @@ def do():
 	#ts,xs,ys=map(list,zip(*pnts[n-3>0 and n-3 or 0:n+1]))
 	ts,xs,ys=map(list,zip(*pnts[0:n+1]))
 	
-	metodo_spline(ts,xs,ys)
+	#metodo_spline(ts,xs,ys)
+	#metodo_CM(ts,xs,ys)
+	metodo_CM2(ts,xs,ys)
 	"""#coef_x=cuadmin(ts+[ts[-1]+1],xs+[arq],4)
 	coef_x=cuadmin(ts,xs,3)
 	xxs=[poli(coef_x,t) for t in range(n+20)]

ca88fc6b0cf2a68db4a261892a2f375c6d184c49
kevinalle <kevinalle@8062f241-9d54-ddaa-24e5-4d4b1e181026>
2010-07-19 22:49:04 +0000

    segfault :)

--- trunk/tp3/NuevoC/arquero.cpp
+++ trunk/tp3/NuevoC/arquero.cpp
@@ -3,6 +3,7 @@
 #include<vector>
 #include<math.h>
 #include"cuadmin.h"
+#include<algorithm>
 using namespace std;
 
 #define forn(i,n) for(int i=0;i<n;i++)
@@ -43,7 +44,6 @@ int go_to(double gol){
 		lastdir=0;
 		to=0;
 	}
-	//move(to);
 	return to;
 }
 
@@ -53,10 +53,10 @@ int main(int argc, const char* argv[]){
 		return 0;
 	}
 	ifstream fin(argv[1]);
-	//ofstream out(argv[2]);
+	ofstream out(argv[2]);
 	int t; double x,y;
 	Vi data_ts; V data_xs,data_ys;
-	Vi ts; V xs,ys;
+	V ts; V xs,ys;
 	while(fin>>t && t>=0){
 		fin >> x >> y;
 		data_ts.push_back(t);
@@ -66,8 +66,6 @@ int main(int argc, const char* argv[]){
 	forsn(i,1,data_ts[0]) move(i,0);
 	forsn(n,data_ts[0],data_ts.back()+1){
 		int curpos=0;
-		//if data[0][0]<=n: pnts.append(data.pop(0))
-		//ts,xs,ys=zip(*pnts)
 		if(data_ts[curpos]<=n){
 			ts.push_back(data_ts[curpos]);
 			xs.push_back(data_xs[curpos]);
@@ -75,43 +73,70 @@ int main(int argc, const char* argv[]){
 			curpos++;
 		}
 
-		//if len(ts)>=4:
 		if(ts.size()>=4){
-			//cxa=cuadmin(ts,xs,min(6,len(ts)-1))
-			//cya=cuadmin(ts,ys,min(6,len(ts)-1))
+			V cxa( CM(ts,xs,min(6,(int)ts.size()-1)) );
+			V cya( CM(ts,ys,min(6,(int)ts.size()-1)) );
 			//ts=ts[-6:]
+			ts.erase(ts.begin(),ts.end()-6);
 			//xs=[poli(cxa,t) for t in ts]
 			//ys=[poli(cya,t) for t in ts]
+			V xxs; forall(t,ts) xxs.push_back(poli(cxa,*t));
+			V yys; forall(t,ts) yys.push_back(poli(cya,*t));
 
 			//cx=cuadmin(ts,xs,2)
 			//cy=cuadmin(ts,ys,2)
+			V cx( CM(ts,xxs,2) );
+			V cy( CM(ts,yys,2) );
+			
 			//xs=[poli(cx,t) for t in ts]
 			//ys=[poli(cy,t) for t in ts]
+			V xxxs; forall(t,ts) xxxs.push_back(poli(cx,*t));
+			V yyys; forall(t,ts) yyys.push_back(poli(cy,*t));
 
 			//t=ts[-1]
+			int t=ts.back();
 			//y=ys[-1]
+			double y=yyys.back();
 			//yans=ys[-2]
+			double yans=*(yyys.end()-1);
 			//while y>0 and t<200:
 				//t+=1
 				//yans=y
 				//y=poli(cy,t)
+			while(y>0 && t<200){
+				t++;
+				yans=y;
+				y=poli(cy,t);
+			}
 			//gol=poli(cx,t+y/(yans-y))
+			double gol=poli(cx,t+y/(yans-y));
 			//goto(gol)
-			move(n,go_to(1));
+			move(n,go_to(gol));
 		//elif len(ts)>1:
 		}else if(ts.size()>1){
 			//cx=cuadmin(ts,xs,len(ts)-1)
 			//cy=cuadmin(ts,ys,len(ts)-1)
+			V cx( CM(ts,xs,(int)ts.size()-1) );
+			V cy( CM(ts,ys,(int)ts.size()-1) );
 			//t=ts[-1]
+			int t=ts.back();
 			//y=ys[-1]
+			double y=ys.back();
 			//yans=ys[-2]
+			double yans=*(ys.end()-1);
 			//while y>0 and t<200:
 				//t+=1
 				//yans=y
 				//y=poli(cy,t)
+			while(y>0 && t<200){
+				t++;
+				yans=y;
+				y=poli(cy,t);
+			}
 			//gol=poli(cx,t+y/(yans-y))
+			double gol=poli(cx,t+y/(yans-y));
 			//goto(gol)
-			move(n,go_to(-1));
+			move(n,go_to(gol));
 		//else: move(0)
 		}else{
 			move(n,0);

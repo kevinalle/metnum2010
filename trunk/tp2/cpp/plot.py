71a9c68548934d72e4fb9296853371677e7f2c90
kevinalle <kevinalle@8062f241-9d54-ddaa-24e5-4d4b1e181026>
2010-07-23 03:58:37 +0000

    relative plot

--- trunk/tp2/cpp/plot.py
+++ trunk/tp2/cpp/plot.py
@@ -29,15 +29,32 @@ def anota(pnt,txt,color=(1.0, 0.7, 0.7)):
         annotate(txt, xy=pnt,  xycoords='data',xytext=(15, 0), textcoords='offset points',size=8,va="center",bbox=dict(boxstyle="round", fc=color, ec="none"), arrowprops=dict(arrowstyle="wedge,tail_width=.5", fc=color, ec="none",patchA=None, relpos=(0.2, 0.7), connectionstyle="arc3,rad=-0.1"))
         #annotate(txt, xy=pnt,  xycoords='data', xytext=(0, -45), textcoords='offset points', size=20, bbox=dict(boxstyle="round", fc=(1.0, 0.7, 0.7), ec=(1., .5, .5)), arrowprops=dict(arrowstyle="wedge,tail_width=1.", fc=(1.0, 0.7, 0.7), ec=(1., .5, .5),patchA=None, relpos=(0.2, 0.8), connectionstyle="arc3,rad=-0.1"))
 
+def find(f, seq):
+	"""Return index of first item in sequence where f(item) == True."""
+	for num,item in enumerate(seq):
+		if f(item): return num
+	return -1
+
 cuerpos=stdin.readline().split()
 N=len(cuerpos)#min(len(cuerpos),len(argv)>1 and int(argv[1]) or len(cuerpos))
 colors=[(.8,.8,.0),(1.,.2,.2),(.3,.8,.7),(.1,.7,.2),(.7,.7,.7),(.7,.0,.0),(.5,.5,.0),(.3,.3,.0)]+[(random(),random(),random()) for _ in range(N-8)]
 posiciones=map(list,zip(*[map(eval,l.split()) for l in stdin.read().strip().split("\n")]))[:N]
 
+#print posiciones
+
+relative_object=find(lambda x:'*' in x, cuerpos)
+if relative_object>=0:
+	#relativizo las posiciones
+	for c in range(N):
+		if c!=relative_object:
+			for i in range(len(posiciones[c])):
+				posiciones[c][i]=[posiciones[c][i][pos]-posiciones[relative_object][i][pos] for pos in (0,1,2)]
+
 for c in range(N):
-        plot(*zip(*posiciones[c])[:2], label=cuerpos[c] , color=colors[c])
-        plot(posiciones[c][-1][0],posiciones[c][-1][1], 'o', ms=6, color=colors[c], mew=0)
-        if options.nombres: anota(posiciones[c][-1][:2],cuerpos[c] ,colors[c]) #nombres de los planetas
+	if c!=relative_object:
+		plot(*zip(*posiciones[c])[:2], label=cuerpos[c] , color=colors[c])
+		plot(posiciones[c][-1][0],posiciones[c][-1][1], 'o', ms=6, color=colors[c], mew=0)
+		if options.nombres: anota(posiciones[c][-1][:2],cuerpos[c] ,colors[c]) #nombres de los planetas
 
 axes().set_aspect(1.)
 #axis([-1.2,1.2,-1.2,1.2], aspect='scaled') # ver solo el centro
